445fcbe2537acbc5c0c01bbfdf1b42cef93169ea
RobertHJones <roberthuwjones@outlook.com>
2022-03-02 18:19:33 +0000

    font size

--- src/Components/Game-table/index.js
+++ src/Components/Game-table/index.js
@@ -2,6 +2,8 @@ import "./index.css";
 
 // usestate to set the state of the table, initial state as blank and then set it as games
 
+// divide page into two divs inside a div direction column, most of it taken up by the left one like 70% which is for the table, the other side for the image, then in the table space between flex
+
 export default function GameTable() {
   return (
     <table class="thegames">
